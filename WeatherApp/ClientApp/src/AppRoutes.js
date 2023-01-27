40fef9692fa0380cb5ab2e078b5b529bcf8612cb
Quinn Johnson <quinnjohnson24@gmail.com>
2023-01-26 18:10:56 -0600

    Add project files.

---
+++ WeatherApp/ClientApp/src/AppRoutes.js
@@ -0,0 +1,16 @@
+import { Counter } from "./components/Counter";
+import { FetchData } from "./components/FetchData";
+import { Home } from "./components/Home";
+
+const AppRoutes = [
+  {
+    index: true,
+    element: <Home />
+  },
+  {
+    path: '/fetch-data',
+    element: <FetchData />
+  }
+];
+
+export default AppRoutes;
---
+++ WeatherApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WeatherApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        //place data in a class then call the class instead
+        public string date;
+
+        public int temperatureC;
+
+        public int temperatureF;
+
+        public string summary;
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //write a method for setting data
+        public async Task getData()
+        {
+            //send api requst
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("http://api.weatherapi.com/v1/current.json?key=78e51fbcb61a41c0aa605637232601&q=Bismarck&aqi=no");
+            if (response.IsSuccessStatusCode)
+            {
+                //set the data to class variables
+                string textResult = await response.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<dynamic>(textResult);
+                temperatureC = json.current.temp_c;
+                temperatureF = json.current.temp_f;
+                summary = json.current.condition.text;
+                date = "today lol";
+                Console.WriteLine(json);
+            }
+            
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+           await this.getData();
+           WeatherForecast[] forecasts = new WeatherForecast[]{
+                new WeatherForecast
+                {
+                    Date = date,
+                    TemperatureC = temperatureC,
+                    Summary = summary,
+                }
+            };
+            return forecasts;
+        }
+
+
+    }
+}
